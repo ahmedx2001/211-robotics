2ef88a15058383af05aaa62ccb8752e227eeb6d8
DLProgram <DanielLuan425@gmail.com>
2016-01-11 16:51:18 -0500

    PID drive control (only P)

--- 211 COMP/Skills Bot/TBH_working.c
+++ 211 COMP/Skills Bot/TBH_working.c
@@ -16,11 +16,82 @@
 
 #include "TBH_Library.c"
 
+//++++++++++++++++++++PID Stuff++++++++++++++++++++
+
+//pid values
+const float pVal = 1;
+//float iVal = 0;
+//float dVal = 0;
+
+//PID variables
+int error = 0;
+//int sumError = 0;
+//int slope = 0;
+//int lastError = 0;
+
+int pChange = 0;
+//int iChange = 0;
+//int dChange = 0;
+int tChange = 0;
+
+//RPM Calculation Variables
+int lastVal = 0;
+int val = 0;
+int diff = 0;
+int targetDis;
+float currentDis;
+
+float cmPreTic = 0.07;
+
+task pid(){											//PID task
+	while(true){		//pid overwrite
+
+			//++++++++++++++++++++Current dis++++++++++++++++++++
+			val = SensorValue[LeftEncoder];
+
+			currentDis = val * cmPreTic;
+			writeDebugStreamLine("current dis: %d", currentDis);
+			//-----------------End Current dis ------------------
+
+			//get error
+			error = targetDis - currentDis;
+			writeDebugStreamLine("error : %d", error);
+
+			//sumError = sumError + error;
+
+			//p calculations
+			pChange = error * pVal;
+
+			writeDebugStreamLine("p change : %d", pChange);
+
+			//i calculations
+			//iChange = sumError * iVal;
+
+			//d calculations
+			//slope = error - lastError;
+			//dChange = slope * dVal;
+
+			//total pid changes
+			tChange = pChange; //+ iChange + dChange;
+
+			writeDebugStreamLine("t change : %d \n\r", tChange);
+
+			//update motor
+
+			motor[LeftDrive] = tChange;
+
+			//lastError = error;
+			lastVal = val;
+			wait1Msec(50);
+	}//while loop
+}//task
+
 task main()
 {
 
 	// Start the flywheel control task
 	startTask( FwControlTask );
+	startTask( pid );
 
 	bool AutoFeed = false;
 	int AutoIndex = 0;
@@ -40,17 +111,20 @@ task main()
 			FwVelocitySet( 00, 0 );
 
 		//////// DRIVE ////////
-		if(abs(vexRT[Ch3]) > 10 || abs(vexRT[Ch4]) > 10 ||
-			abs(vexRT[Ch1]) > 10 || abs(vexRT[Ch2]) > 10){
-			motor[LeftDrive]  = ((vexRT[Ch2] + vexRT[Ch1])/2 - (vexRT[Ch3] - vexRT[Ch4]));
-			motor[RightDrive]   = ((vexRT[Ch2] - vexRT[Ch1])/2 - (vexRT[Ch3] + vexRT[Ch4]));
-		}
-		else{
-			motor[LeftDrive]  = 0;
-			motor[RightDrive]  = 0;
-		}
+		//if(abs(vexRT[Ch3]) > 10 || abs(vexRT[Ch4]) > 10 ||
+		//	abs(vexRT[Ch1]) > 10 || abs(vexRT[Ch2]) > 10){
+		//	motor[LeftDrive]  = ((vexRT[Ch2] + vexRT[Ch1])/2 - (vexRT[Ch3] - vexRT[Ch4]));
+		//	motor[RightDrive]   = ((vexRT[Ch2] - vexRT[Ch1])/2 - (vexRT[Ch3] + vexRT[Ch4]));
+		//}
+		//else{
+		//	motor[LeftDrive]  = 0;
+		//	motor[RightDrive]  = 0;
+		//}
 
 		/////// INTAKE ////////
+
+			targetDis = vexRT[ch2];
+
 		if(vexRT[Btn6U] == 1){
 			motor[Intake]   = 127;
 		}
